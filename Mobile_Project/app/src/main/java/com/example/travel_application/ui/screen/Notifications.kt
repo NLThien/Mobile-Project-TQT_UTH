d457c0202d53084cc0f41c73ba0863db1e41d8ce
NLThien <thienkd2004@gmail.com>
2025-03-24 04:01:01 +0700

    rebuild : xml to jetpack compose
    # Conflicts:
    #	Mobile_Project/.gradle/8.11.1/checksums/checksums.lock
    #	Mobile_Project/.gradle/8.11.1/executionHistory/executionHistory.lock
    #	Mobile_Project/.gradle/8.11.1/fileHashes/fileHashes.bin
    #	Mobile_Project/.gradle/8.11.1/fileHashes/fileHashes.lock
    #	Mobile_Project/.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	Mobile_Project/.gradle/buildOutputCleanup/cache.properties
    #	Mobile_Project/gradle/libs.versions.toml

---
+++ Mobile_Project/app/src/main/java/com/example/travel_application/ui/screen/Notifications.kt
@@ -0,0 +1,60 @@
+package com.example.travel_application.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.travel_application.R
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import android.R.id.bold
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NotificationsScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+//        Image(
+//            painter = painterResource(id = androidx.compose.foundation.layout.R.drawable.logo),
+//            contentDescription = "Logo",
+//            modifier = Modifier.size(300.dp)
+//        )
+
+        Text("Trang thông báo", fontSize = 30.sp, fontWeight = androidx.compose.ui.text.font.FontWeight.Bold)
+
+        Text(
+            text = "Chứa mọi thông báo của ứng dụng",
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(50.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+//        Button(onClick = { navController.navigate(Screen.ComponentList.route) },
+//            modifier = Modifier
+//                .width(340.dp)
+//                .height(50.dp)
+//        ) {
+//            Text("Push", fontSize = 20.sp)
+//        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun PreviewNotificationsScreen() {
+    val navController = NavController(LocalContext.current)
+    NotificationsScreen(navController = navController)
+}
